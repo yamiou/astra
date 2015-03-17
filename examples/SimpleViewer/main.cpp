b3098dfc9d91774d577eeed9ac484cd352a17ec5
Gordon McShane <gordonmcshane@gmail.com>
2015-03-17 11:42:01 -0400

    fix POSIX compilation of SimpleViewer

--- examples/SimpleViewer/main.cpp
+++ examples/SimpleViewer/main.cpp
@@ -6,7 +6,7 @@
 
 #ifdef _WIN32
 
-#include <windows.h> 
+#include <windows.h>
 
 #else //_WIN32 -> OS X, LINUX
 
@@ -16,32 +16,32 @@
 
 #endif //_WIN32
 
-bool shouldContinue = true;
+static bool shouldContinue = true;
 
 void signal_stop_processing(){
-	printf("quitting...\n\n");
-	shouldContinue = false;
+    printf("quitting...\n\n");
+    shouldContinue = false;
 }
 
 #ifdef _WIN32
 
 BOOL CtrlHandler(DWORD fdwCtrlType)
 {
-	switch (fdwCtrlType)
-	{
-		// Handle the CTRL-C signal. 
-	case CTRL_C_EVENT:
-		signal_stop_processing();
-		return(TRUE);
-	default:
-		return FALSE;
-	}
+    switch (fdwCtrlType)
+    {
+        // Handle the CTRL-C signal.
+    case CTRL_C_EVENT:
+        signal_stop_processing();
+        return(TRUE);
+    default:
+        return FALSE;
+    }
 }
 
 #else //_WIN32 -> OSX, LINUX
 
-void signal_handler(int s){
-	signal_stop_processing();
+static void signal_handler(int s){
+    signal_stop_processing();
 }
 
 #endif //_WIN32
@@ -49,50 +49,53 @@ void signal_handler(int s){
 int main(int argc, char** argv)
 {
 #ifdef _WIN32
-	SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
+    SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
+
 #else //_WIN32 -> OS X, LINUX
-	struct sigaction sigIntHandler;
 
-	sigIntHandler.sa_handler = my_handler;
-	sigemptyset(&sigIntHandler.sa_mask);
-	sigIntHandler.sa_flags = 0;
+    struct sigaction sigIntHandler;
 
-	sigaction(SIGINT, &sigIntHandler, NULL);
+    sigIntHandler.sa_handler = &signal_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
 #endif //_WIN32
 
-	sensekit_initialize();
+    sensekit_initialize();
 
-	sensekit_sensor_t* sensor;
-	sensekit_status_t status = SENSEKIT_STATUS_SUCCESS;
+    sensekit_sensor_t* sensor;
+    sensekit_status_t status = SENSEKIT_STATUS_SUCCESS;
 
-	//client connects to daemon host, registers interest in certain sensor URI
-	status = sensekit_open_sensor("1d27/0601@20/30", &sensor);
+    //client connects to daemon host, registers interest in certain sensor URI
+    status = sensekit_open_sensor("1d27/0601@20/30", &sensor);
 
-	sensekit_depthstream_t* depthStream;
-	//client -> daemon resolves stream type to plugin, notifies plugin client added
-	//client service starts pulling (or daemon starts pushing) when data is available
-	//client service stores latest frame until requested (via open or event)
-	status = sensekit_depth_open(sensor, &depthStream);
+    sensekit_depthstream_t* depthStream;
+    //client -> daemon resolves stream type to plugin, notifies plugin client added
+    //client service starts pulling (or daemon starts pushing) when data is available
+    //client service stores latest frame until requested (via open or event)
+    status = sensekit_depth_open(sensor, &depthStream);
 
-	do
-	{
-		//Needed for now until backend plugins have their own thread and we figure out a thread/dispatch model
-		sensekit_temp_update();
+    do
+    {
+        //Needed for now until backend plugins have their own thread and we figure out a thread/dispatch model
+        sensekit_temp_update();
 
-		sensekit_depthframe_t* depthFrame;
-		//
-		sensekit_depth_frame_open(depthStream,
-								  30, depthFrame);
+        sensekit_depthframe_t* depthFrame;
+        //
+        sensekit_depth_frame_open(depthStream,
+                                  30, depthFrame);
 
-		std::cout << "index: " << depthFrame->frameIndex << " value: " << depthFrame->sampleValue << std::endl;
+        std::cout << "index: " << depthFrame->frameIndex << " value: " << depthFrame->sampleValue << std::endl;
 
-		sensekit_depth_frame_close(depthFrame);
+        sensekit_depth_frame_close(depthFrame);
 
-	} while (shouldContinue);
+    } while (shouldContinue);
 
-	status = sensekit_depth_close(&depthStream);
+    status = sensekit_depth_close(&depthStream);
 
-	status = sensekit_close_sensor(&sensor);
+    status = sensekit_close_sensor(&sensor);
 
-	sensekit_terminate();
+    sensekit_terminate();
 }
