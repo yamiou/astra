07cdcc30fcd4c1063706b23a8af4856abaef174c
Gordon McShane <gordonmcshane@gmail.com>
2015-04-10 00:25:26 -0400

    P = NP finally solved. Also more StreamDescription work.

--- include/SenseKit/SenseKit.h
+++ include/SenseKit/SenseKit.h
@@ -48,27 +48,37 @@ namespace sensekit {
         friend class StreamReader;
     };
 
-    class StreamDescription : private sensekit_stream_desc_t
+    class StreamDescription
     {
     public:
         StreamDescription(sensekit_stream_type_t type,
                           sensekit_stream_subtype_t subType)
             {
-                this->type = type;
-                this->subType = subType;
+                m_desc.type = type;
+                m_desc.subType = subType;
             }
 
-        sensekit_stream_type_t get_type() { return type; }
-        sensekit_stream_subtype_t get_subType() { return subType; }
+        StreamDescription(const sensekit_stream_desc_t& desc)
+            : m_desc(desc)
+            { }
 
-        friend class DataStream;
-        friend bool operator==(const StreamDescription& lhs, const StreamDescription& rhs);
-        friend bool operator!=(const StreamDescription& lhs, const StreamDescription& rhs);
+        StreamDescription operator=(const sensekit_stream_desc_t& desc)
+            {
+                return StreamDescription(desc);
+            }
+
+        const sensekit_stream_desc_t& get_desc_t() const { return m_desc; }
+
+        sensekit_stream_type_t get_type() const { return m_desc.type; }
+        sensekit_stream_subtype_t get_subType() const { return m_desc.subType; }
+
+    private:
+        sensekit_stream_desc_t m_desc;
     };
 
     inline bool operator==(const StreamDescription& lhs, const StreamDescription& rhs)
     {
-        return lhs.type == rhs.type && lhs.subType == rhs.subType;
+        return lhs.get_type() == rhs.get_type() && lhs.get_subType() == rhs.get_subType();
     }
 
     inline bool operator!=(const StreamDescription& lhs, const StreamDescription& rhs)
