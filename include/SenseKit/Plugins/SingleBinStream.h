2d8283e64204033c3a735bb27cd529450727d521
Joshua Blake <josh@orbbec.com>
2015-04-19 22:44:34 -0400

    SingleBinStream whitespace

--- include/SenseKit/Plugins/SingleBinStream.h
+++ include/SenseKit/Plugins/SingleBinStream.h
@@ -5,68 +5,68 @@
 
 namespace sensekit { namespace plugins {
 
-        template<typename TFrameType, typename TBlockType>
-        class SingleBinStream : public Stream
-        {
-        public:
+    template<typename TFrameType, typename TBlockType>
+    class SingleBinStream : public Stream
+    {
+    public:
 
-            SingleBinStream(PluginServiceProxy& pluginService,
-                            Sensor& streamSet,
-                            StreamDescription description,
-                            size_t bufferSize)
-                : Stream(pluginService,
-                         streamSet,
-                         description)
-                {
-                    create_bin(sizeof(TFrameType) + bufferSize, m_binHandle, m_frame);
-                }
+        SingleBinStream(PluginServiceProxy& pluginService,
+                        Sensor& streamSet,
+                        StreamDescription description,
+                        size_t bufferSize)
+            : Stream(pluginService,
+                        streamSet,
+                        description)
+            {
+                create_bin(sizeof(TFrameType) + bufferSize, m_binHandle, m_frame);
+            }
 
-            using frame_type = TFrameType;
-            using block_type = TBlockType;
+        using frame_type = TFrameType;
+        using block_type = TBlockType;
 
-            bool has_connections()
-                {
-                    if (m_binHandle == nullptr)
-                        return false;
+        bool has_connections()
+            {
+                if (m_binHandle == nullptr)
+                    return false;
 
-                    return bin_has_connections(m_binHandle);
-                }
+                return bin_has_connections(m_binHandle);
+            }
 
-            frame_type* begin_write(size_t frameIndex)
-                {
-                    if (m_locked)
-                        return reinterpret_cast<TFrameType*>(m_frame->data);
-
-                    m_locked = true;
-                    m_frame->frameIndex = frameIndex;
+        frame_type* begin_write(size_t frameIndex)
+            {
+                if (m_locked)
                     return reinterpret_cast<TFrameType*>(m_frame->data);
-                }
 
-            void end_write()
-                {
-                    if (!m_locked)
-                        return;
+                m_locked = true;
+                m_frame->frameIndex = frameIndex;
+                return reinterpret_cast<TFrameType*>(m_frame->data);
+            }
 
-                    cycle_bin(m_binHandle, m_frame);
-                    m_locked = false;
-                }
+        void end_write()
+            {
+                if (!m_locked)
+                    return;
 
-            virtual void on_connection_added(sensekit_streamconnection_t connection) override
-                {
-                    link_connection_to_bin(connection, m_binHandle);
-                }
+                cycle_bin(m_binHandle, m_frame);
+                m_locked = false;
+            }
 
-            virtual void on_connection_removed(sensekit_bin_t bin,
-                                               sensekit_streamconnection_t connection) override
-                {
-                    link_connection_to_bin(connection, nullptr);
-                }
+        virtual void on_connection_added(sensekit_streamconnection_t connection) override
+            {
+                link_connection_to_bin(connection, m_binHandle);
+            }
 
-        private:
-            bool m_locked{false};
-            sensekit_bin_t m_binHandle{nullptr};
-            sensekit_frame_t* m_frame{nullptr};
-        };
-    }}
+        virtual void on_connection_removed(sensekit_bin_t bin,
+                                            sensekit_streamconnection_t connection) override
+            {
+                link_connection_to_bin(connection, nullptr);
+            }
+
+    private:
+        bool m_locked{false};
+        sensekit_bin_t m_binHandle{nullptr};
+        sensekit_frame_t* m_frame{nullptr};
+    };
+}}
 
 #endif /* SINGLEBINSTREAM_H */
