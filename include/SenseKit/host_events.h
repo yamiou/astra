98e0b0864157910615b92da4d4d16ced668e7bec
Gordon McShane <gordonmcshane@gmail.com>
2015-05-27 12:18:10 -0400

    Fix linkage on Windows

--- include/SenseKit/host_events.h
+++ include/SenseKit/host_events.h
@@ -8,9 +8,9 @@
 
 SENSEKIT_BEGIN_DECLS
 
-sensekit_status_t sensekit_notify_host_event(sensekit_event_id id, const void* data, size_t dataSize);
-sensekit_status_t sensekit_notify_resource_available(const char* resourceURI);
-sensekit_status_t sensekit_notify_resource_unavailable(const char* resourceURI);
+SENSEKIT_API sensekit_status_t sensekit_notify_host_event(sensekit_event_id id, const void* data, size_t dataSize);
+SENSEKIT_API sensekit_status_t sensekit_notify_resource_available(const char* resourceURI);
+SENSEKIT_API sensekit_status_t sensekit_notify_resource_unavailable(const char* resourceURI);
 
 enum {
     SENSEKIT_EVENT_RESOURCE_AVAILABLE = 1,
