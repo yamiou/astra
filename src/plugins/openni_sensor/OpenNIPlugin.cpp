0bb307bf7444fc61d160596b49e7b552207deabd
Gordon McShane <gordonmcshane@gmail.com>
2015-06-02 00:12:16 -0400

    support delayed activation of sensors on Android

--- src/plugins/openni_sensor/OpenNIPlugin.cpp
+++ src/plugins/openni_sensor/OpenNIPlugin.cpp
@@ -4,6 +4,7 @@
 #include "OniDepthStream.h"
 #include "OniColorStream.h"
 #include <Shiny.h>
+#include <cstdio>
 
 EXPORT_PLUGIN(sensekit::plugins::OpenNIPlugin)
 
@@ -22,12 +23,12 @@ namespace sensekit
                               version.maintenance,
                               version.build);
 
-            ::openni::Status rc = ::openni::STATUS_OK;
+            openni::Status rc = ::openni::STATUS_OK;
 
-            ::openni::OpenNI::addDeviceConnectedListener(this);
-            ::openni::OpenNI::addDeviceDisconnectedListener(this);
+            openni::OpenNI::addDeviceConnectedListener(this);
+            openni::OpenNI::addDeviceDisconnectedListener(this);
 
-            rc = ::openni::OpenNI::initialize();
+            rc = openni::OpenNI::initialize();
 
             bool successful = rc == openni::STATUS_OK;
 
@@ -48,22 +49,53 @@ namespace sensekit
         void OpenNIPlugin::on_host_event(sensekit_event_id id, const void* data, size_t dataSize)
         {
             PROFILE_FUNC();
+#ifdef __ANDROID__
             switch (id)
             {
             case SENSEKIT_EVENT_RESOURCE_AVAILABLE:
-                get_logger().info("test: %s", data);
+                const char* resourceUri = static_cast<const char*>(data);
+
+                unsigned int vid = 0;
+                unsigned int pid = 0;
+                unsigned int bus = 0;
+                unsigned int address = 0;
+
+                sscanf(resourceUri, "usb/%u/%u/%u/%u", &vid, &pid, &bus, &address);
+
+                char oniUri[1024];
+                snprintf(oniUri, 1024, "%.4x/%.4x@%x/%x", vid, pid, bus, address);
+
+                openni::Array<openni::DeviceInfo> devices;
+                openni::OpenNI::enumerateDevices(&devices);
+
+                for(int i = 0; i < devices.getSize(); i++)
+                {
+                    const openni::DeviceInfo& info = devices[i];
+                    if (strcmp(oniUri, info.getUri()) == 0)
+                    {
+                        get_logger().info("device connected: %s", info.getUri());
+
+                        OniDeviceStreamSet* set = new OniDeviceStreamSet(get_pluginService(), &info);
+                        set->open();
+
+                        m_sets.push_back(SetPtr(set));
+                    }
+                }
             }
+#endif
         }
 
         void OpenNIPlugin::onDeviceConnected(const ::openni::DeviceInfo* info)
         {
             PROFILE_FUNC();
+#ifndef __ANDROID__
             get_logger().info("device connected: %s", info->getUri());
 
             OniDeviceStreamSet* set = new OniDeviceStreamSet(get_pluginService(), info);
             set->open();
 
             m_sets.push_back(SetPtr(set));
+#endif
         }
 
         void OpenNIPlugin::onDeviceDisconnected(const ::openni::DeviceInfo* info)
