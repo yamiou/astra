2d060ecd8f7064aa6e8402f1b4eb85390a156314
gezai <zenglingbin@orbbec.com>
2015-08-14 20:59:00 +0800

    test depth and color stream in cpp

--- copy.bat
+++ copy.bat
@@ -1,2 +1,2 @@
-copy .\samples-aux\vs2013\thirdparty\copy_to_bin_dir\*.* .\build-vs\bin\Debug\
-copy .\samples-aux\vs2013\thirdparty\copy_to_bin_dir\*.* .\build-vs\bin\Release\
+xcopy .\samples-aux\vs2013\thirdparty\copy_to_bin_dir\*.* .\build-vs\bin\Debug\/e
+xcopy .\samples-aux\vs2013\thirdparty\copy_to_bin_dir\*.* .\build-vs\bin\Release\/e
--- update_proj.bat
+++ update_proj.bat
@@ -1,2 +1,7 @@
-cd build-vs
+if not exist build mkdir build
+cd build
+
+REM Update your dependency directories below as necessary
+
 cmake -G"Visual Studio 12" -DOpenCV_DIR="C:\libs\opencv-2.4.10\build" -DGLUT_ROOT_PATH="C:\libs\glut_windows" -DSFML_ROOT="C:\libs\SFML-2.2" -DPROTOBUF_SRC_ROOT_FOLDER="C:\libs\protobuf-2.6.1" ..
+cd ..
