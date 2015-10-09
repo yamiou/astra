e9d8c536590f1bfd673a5dc2f0cb69e31fd6757a
Joshua Blake <josh@orbbec3d.com>
2015-10-09 15:05:38 -0700

    Various changes to support Win64: * Move InstallVS.cmake.in to samples-aux * Move configured InstallVS.cmake * Update gen_vs2013_proj_win[32|64].bat to generate appropriate project arch * Add CMakeCache.txt.[x86|x64].in for 32- or 64-bit sample project generation * Update SFML dependencies in vs2013 submodule, adding 32 and 64-bit variations

--- gen_vs2013_proj_win32.bat
+++ gen_vs2013_proj_win32.bat
@@ -1,11 +1,19 @@
+@echo off
 if not exist build mkdir build
 cd build
 
 REM Update your dependency directories below as necessary
 
-cmake -G"Visual Studio 12" -DOpenCV_DIR="C:\libs\opencv-2.4.10\build" -DSFML_ROOT="C:\libs\SFML-2.2" -DPROTOBUF_SRC_ROOT_FOLDER="C:\libs\protobuf-2.6.1" ..
-cd ..
+cmake -G"Visual Studio 12" -DOpenCV_DIR="C:\libs\opencv-2.4.10\build" -DSFML_ROOT="C:\libs\SFML-2.2" -DPROTOBUF_SRC_ROOT_FOLDER="C:\libs\protobuf-2.6.1" .. && goto :copy_files
 
-xcopy .\samples-aux\vs2013\copy_to_bin32_dir\*.* .\build\bin\Debug\ /E /Y
-xcopy .\samples-aux\vs2013\copy_to_bin32_dir\*.* .\build\bin\Release\ /E /Y
-xcopy .\samples-aux\vs2013\copy_to_bin32_dir\*.* .\build\bin\RelWithDebInfo\ /E /Y
+REM only if cmake failed...
+GOTO end
+
+:copy_files
+xcopy ..\samples-aux\vs2013\copy_to_bin32_dir\*.* .\bin\Debug\ /E /Y
+xcopy ..\samples-aux\vs2013\copy_to_bin32_dir\*.* .\bin\Release\ /E /Y
+xcopy ..\samples-aux\vs2013\copy_to_bin32_dir\*.* .\bin\RelWithDebInfo\ /E /Y
+echo Done!
+
+:end
+cd ..
