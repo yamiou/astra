e39fff072d88abccf9b423edcb40331a1e64a4a0
Gordon McShane <gordonmcshane@gmail.com>
2015-10-23 18:28:56 -0700

    adjust parameter alignment

--- include/astra/capi/streams/skeleton_capi.h
+++ include/astra/capi/streams/skeleton_capi.h
@@ -24,23 +24,23 @@
 ASTRA_BEGIN_DECLS
 
 ASTRA_API_EX astra_status_t astra_reader_get_skeletonstream(astra_reader_t reader,
-                                                                     astra_skeletonstream_t* skeletonStream);
+                                                            astra_skeletonstream_t* skeletonStream);
 
 ASTRA_API_EX astra_status_t astra_frame_get_skeletonframe(astra_reader_frame_t readerFrame,
-                                                                   astra_skeletonframe_t* skeletonFrame);
+                                                          astra_skeletonframe_t* skeletonFrame);
 
 ASTRA_API_EX astra_status_t astra_skeletonframe_get_frameindex(astra_skeletonframe_t skeletonFrame,
-                                                                        astra_frame_index_t* index);
+                                                               astra_frame_index_t* index);
 
 ASTRA_API_EX astra_status_t astra_skeletonframe_get_skeleton_count(astra_skeletonframe_t skeletonFrame,
-                                                                            size_t* skeletonCount);
+                                                                   size_t* skeletonCount);
 
 ASTRA_API_EX astra_status_t astra_skeletonframe_copy_skeletons(astra_skeletonframe_t skeletonFrame,
-                                                                        astra_skeleton_t* skeletonsDestination);
+                                                               astra_skeleton_t* skeletonsDestination);
 
 ASTRA_API_EX astra_status_t astra_skeletonframe_get_skeletons_ptr(astra_skeletonframe_t skeletonFrame,
-                                                                           astra_skeleton_t** skeletons,
-                                                                           size_t* skeletonCount);
+                                                                  astra_skeleton_t** skeletons,
+                                                                  size_t* skeletonCount);
 
 ASTRA_END_DECLS
 
--- src/astra/astra_skeleton.cpp
+++ src/astra/astra_skeleton.cpp
@@ -27,32 +27,32 @@
 ASTRA_BEGIN_DECLS
 
 ASTRA_API_EX astra_status_t astra_reader_get_skeletonstream(astra_reader_t reader,
-                                                                     astra_skeletonstream_t* skeletonStream)
+                                                            astra_skeletonstream_t* skeletonStream)
 
 {
     return astra_reader_get_stream(reader,
-                                      ASTRA_STREAM_SKELETON,
-                                      DEFAULT_SUBTYPE,
-                                      skeletonStream);
+                                   ASTRA_STREAM_SKELETON,
+                                   DEFAULT_SUBTYPE,
+                                   skeletonStream);
 }
 
 ASTRA_API_EX astra_status_t astra_frame_get_skeletonframe(astra_reader_frame_t readerFrame,
-                                                                   astra_skeletonframe_t* skeletonFrame)
+                                                          astra_skeletonframe_t* skeletonFrame)
 {
     return astra_generic_frame_get<astra_skeletonframe_wrapper_t>(readerFrame,
-                                                                        ASTRA_STREAM_SKELETON,
-                                                                        DEFAULT_SUBTYPE,
-                                                                        skeletonFrame);
+                                                                  ASTRA_STREAM_SKELETON,
+                                                                  DEFAULT_SUBTYPE,
+                                                                  skeletonFrame);
 }
 
 ASTRA_API_EX astra_status_t astra_skeletonframe_get_frameindex(astra_skeletonframe_t skeletonFrame,
-                                                                        astra_frame_index_t* index)
+                                                               astra_frame_index_t* index)
 {
     return astra_generic_frame_get_frameindex(skeletonFrame, index);
 }
 
 ASTRA_API_EX astra_status_t astra_skeletonframe_get_skeleton_count(astra_skeletonframe_t skeletonFrame,
-                                                                            size_t* skeletonCount)
+                                                                   size_t* skeletonCount)
 {
     *skeletonCount = skeletonFrame->skeletonCount;
 
@@ -60,7 +60,7 @@ ASTRA_API_EX astra_status_t astra_skeletonframe_get_skeleton_count(astra_skeleto
 }
 
 ASTRA_API_EX astra_status_t astra_skeletonframe_copy_skeletons(astra_skeletonframe_t skeletonFrame,
-                                                                        astra_skeleton_t* skeletonsDestination)
+                                                               astra_skeleton_t* skeletonsDestination)
 {
     size_t size = skeletonFrame->skeletonCount * sizeof(astra_skeleton_t);
 
@@ -70,8 +70,8 @@ ASTRA_API_EX astra_status_t astra_skeletonframe_copy_skeletons(astra_skeletonfra
 }
 
 ASTRA_API_EX astra_status_t astra_skeletonframe_get_skeletons_ptr(astra_skeletonframe_t skeletonFrame,
-                                                                           astra_skeleton_t** skeletons,
-                                                                           size_t* skeletonCount)
+                                                                  astra_skeleton_t** skeletons,
+                                                                  size_t* skeletonCount)
 {
     *skeletons = skeletonFrame->skeletons;
     astra_skeletonframe_get_skeleton_count(skeletonFrame, skeletonCount);
