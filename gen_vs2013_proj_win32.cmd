dd4e866a90c3121c9db7e2c4370fd45d72219599
Gordon McShane <gordonmcshane@gmail.com>
2016-04-19 16:48:47 -0700

    Refactor VS project generator scripts
    - common behavior in generate.cmd script
    - cmd extension so ERRORLEVELs are respected

---
+++ gen_vs2013_proj_win32.cmd
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+SETLOCAL EnableExtensions EnableDelayedExpansion
+
+REM Update your dependency directories below as necessary
+SET SFMLPATH=C:\libs\SFML-2.3.2\vs2013\x86
+SET PROTOPATH=C:\libs\protobuf-2.6.1\vs2013
+
+SET MSVC=12
+SET ARCH=x86
+SET EXTRAARGS=%*
+
+CALL %~dp0\generate.cmd
